3b77bb2f084f98fc5859d4453e900996cd92a456
sddxldq <sddxldq@gmail.com>
2021-09-21 00:38:58 -0700

    milestone

--- index.js
+++ index.js
@@ -7,21 +7,44 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/post");
+const multer = require("multer");
+const path = require("path");
+const { use } = require("./routes/post");
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, () => {
-  console.log("Connected to MongoDB");
+	console.log("Connected to MongoDB");
 });
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, "public/images");
+	},
+	filename: (req, file, cb) => {
+		cb(null, req.body.name);
+	},
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+	try {
+		return res.status(200).json("File uploaded successfully");
+	} catch (err) {
+		console.log(err);
+	}
+});
 
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
 
 app.listen(8800, () => {
-  console.log("Backend server is running! hahaha");
+	console.log("Backend server is running! hahaha");
 });
--- routes/auth.js
+++ routes/auth.js
@@ -3,40 +3,39 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
 router.post("/register", async (req, res) => {
-  try {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+	try {
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: hashedPassword,
-    });
+		const newUser = new User({
+			username: req.body.username,
+			email: req.body.email,
+			password: hashedPassword,
+		});
 
-    const user = await newUser.save();
-    res.status(200).json(user);
-  } catch (err) {
-    console.log(err);
-  }
+		const user = await newUser.save();
+		res.status(200).json(user);
+	} catch (err) {
+		console.log(err);
+	}
 });
 
 router.post("/login", async (req, res) => {
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    !user
-      && res.status(404).json("user not found")
-      // : res.status(200).json(`${user.username}`);
+	try {
+		const user = await User.findOne({ email: req.body.email });
+		!user && res.status(404).json("user not found");
+		// : res.status(200).json(`${user.username}`);
 
-    const validPassword = await bcrypt.compare(
-      req.body.password,
-      user.password
-    );
-    !validPassword
-      ? res.status(400).json("wrong password")
-      : res.status(200).json(`${user.username}`+" Welcome back");
-  } catch (err) {
-    console.log(err);
-  }
+		const validPassword = await bcrypt.compare(
+			req.body.password,
+			user.password
+		);
+		!validPassword
+			? res.status(400).json("wrong password")
+			: res.status(200).json(user);
+	} catch (err) {
+		console.log(err);
+	}
 });
 
 module.exports = router;
